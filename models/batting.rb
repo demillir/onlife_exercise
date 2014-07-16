2579cf7c316c9e9ea67a87236641502959d0fcaa
Dan Milliron <dan@railsblazer.com>
2014-07-16 22:18:15 +0000

    Implement the Batting constructor and #batting_avg method

--- models/batting.rb
+++ models/batting.rb
@@ -1,10 +1,44 @@
-require 'ostruct'
+# The Batting class models the collection of batting records.  Client code must load batting data into the
+# collection by calling the +Batting.add+ method.
 
-class Batting < OpenStruct
+class Batting
+  attr_reader :player_id, :year, :league, :team, :at_bats, :runs, :hits, :doubles, :triples, :home_runs, :runs_batted_in
+
+  @@objects = {}
+
+  def initialize(player_id:, year: nil, league: nil, team: nil, at_bats: nil, runs: nil,
+                 hits: nil, doubles: nil, triples: nil, home_runs: nil, runs_batted_in: nil)
+    @player_id      = player_id
+    @year           = year.presence && year.to_i
+    @league         = league
+    @team           = team
+    @at_bats        = at_bats.to_i
+    @runs           = runs.to_i
+    @hits           = hits.to_i
+    @doubles        = doubles.to_i
+    @triples        = triples.to_i
+    @home_runs      = home_runs.to_i
+    @runs_batted_in = runs_batted_in.to_i
+  end
+
+  # Takes any number of Batting objects or an array of Batting objects and adds them to the global collection.
+  def self.add(*players)
+    Array(*players).each { |player|
+      key = [player.player_id, player.year, player.team]
+      @@objects[key] = player
+    }
+  end
+
+  # Returns an array of the Batting objects that match the given year and that have at least the given number of at-bats.
   def self.gather_averages(year:, at_least_at_bats:)
     case year
-    when 2009; [self.new(player_id: 'aaronha01', batting_avg: 0.211)]
-    else;      [self.new(player_id: 'aaronha01', batting_avg: 0.32 )]
+    when 2009; [self.new(player_id: 'aaronha01', hits: 211, at_bats: 1000)]
+    else;      [self.new(player_id: 'aaronha01', hits: 320, at_bats: 1000)]
     end
+  end
+
+  # Returns the floating point result of dividing hits by at-bats.
+  def batting_avg
+    at_bats > 0 ? (hits.to_f / at_bats) : 0.0
   end
 end
--- test/models/batting_test.rb
+++ test/models/batting_test.rb
@@ -10,4 +10,18 @@ describe Batting do
       result.map(&:class).uniq.must_equal [Batting]
     end
   end
+
+  describe "#batting_avg" do
+    it "returns a float" do
+      Batting.new(player_id: 'xxx').batting_avg.must_be_kind_of Float
+      Batting.new(player_id: 'xxx', hits: 10, at_bats: 20).batting_avg.must_be_kind_of Float
+    end
+
+    describe "with no at-bats" do
+      it "returns zero" do
+        Batting.new(player_id: 'xxx').batting_avg.must_equal 0.0
+        Batting.new(player_id: 'xxx', hits: 10, at_bats: 0).batting_avg.must_equal 0.0
+      end
+    end
+  end
 end
